7e67f09b5962fb3ebeb3ecc49d4baf58ec496e5d
Flurin Egger <flurin@digitpaint.nl>
2012-12-12 08:46:52 +0100

    Prep for 12

--- html/preparation/12/12.js
+++ html/preparation/12/12.js
@@ -14,3 +14,16 @@ Digitpaint HTML and CSS Advent 2012
 Copyright 2012 by Digitpaint. This code is licensed under the MIT License.
 
 */  
+
+document.addEventListener("DOMContentLoaded", function(){
+  navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia;
+  window.URL = window.webkitURL ||  window.URL;
+  
+  var cam = document.getElementById("cam");
+  
+  // Prepare the buffer
+  var buffer = document.createElement("canvas");
+  var ctxB = buffer.getContext("2d");
+  
+  
+});
