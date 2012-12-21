ab53d666ffaee3c7afa27519dfb4b4eaa61e28ed
Flurin Egger <flurin@digitpaint.nl>
2012-12-21 22:46:14 +0100

    Prepare for 22nd

--- html/preparation/22/22.js
+++ html/preparation/22/22.js
@@ -15,57 +15,114 @@ Copyright 2012 by Digitpaint. This code is licensed under the MIT License.
 */  
 
 document.addEventListener("DOMContentLoaded", function(){
+  var lastEnter = null;
   
-  function handleDragStart(e) {
+  
+  var handleDragStart = function(e) {
     this.style.opacity = '0.4';  // this / e.target is the source node.
+    
+    // This is for internal use only
     e.dataTransfer.setData("domid", this.id);
+    
+    // If you drag this out there we add the category too.
+    var category = this.parentNode.previousElementSibling.innerText;
+    e.dataTransfer.setData("text/plain", category + " : " + this.innerText);
+    e.dataTransfer.setData("text/html", "<strong>" + category + "</strong> : " + this.innerHTML);    
+
   }
 
-  function handleDragOver(e) {
-    if (e.preventDefault) {
-      e.preventDefault(); // Necessary. Allows us to drop.
-    }
+  var handleDragOver = function(e) {
+    e.preventDefault(); // Necessary. Allows us to drop.
 
     e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    
+    // Hack to counter handleDragLeave being called when entering child nodes.
+    this.classList.add('over');
     return false;
   }
 
-  function handleDragEnter(e) {
+  var handleDragEnter = function(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation(); // stops the browser from redirecting.
+    }    
     // this / e.target is the current hover target.
     this.classList.add('over');
   }
 
-  function handleDragLeave(e) {
-    this.classList.remove('over');  // this / e.target is previous target element.
+  var handleDragLeave = function(e) {
+    if (e.stopPropagation) {
+      e.stopPropagation(); 
+    }
+    
+    this.classList.remove('over');
   }
   
-  function handleDrop(e) {
+  var handleDrop = function(e) {
     // this / e.target is current target element.
 
     if (e.stopPropagation) {
       e.stopPropagation(); // stops the browser from redirecting.
     }
+    e.preventDefault();
 
     // See the section on the DataTransfer object.
-    console.log("huh?", e.dataTransfer.getData("domid"));
+    var domId = e.dataTransfer.getData("domid");
+    var el;
     
+    // if(e.dataTransfer.types && e.dataTransfer.types.length > 0){
+    //   for(var i=0; i < e.dataTransfer.types.length; i++){
+    //     console.log(e.dataTransfer.types[i], e.dataTransfer.getData(e.dataTransfer.types[i]));        
+    //   }
+    // }
+    
+    if(domId){
+      el = document.getElementById(e.dataTransfer.getData("domid"));
+      this.appendChild(el);
+    } else {
+
+      if(e.dataTransfer.files.length > 0){
+        for(var i=0; i < e.dataTransfer.files.length; i++){
+          el = createChild(e.dataTransfer.files[i].name);
+          this.appendChild(el);
+        }
+      } else {
+        el = createChild(e.dataTransfer.getData(e.dataTransfer.types && e.dataTransfer.types[0] || "Text"));
+        this.appendChild(el);
+      }
+    }
+    
+    // Cleanup (this shouldn't have to be done but well...)
+    this.classList.remove("over");
 
     return false;
   }
 
-  function handleDragEnd(e) {
+  var handleDragEnd = function(e) {
     // this/e.target is the source node.
     this.style.opacity = "1";
 
-    [].forEach.call(cols, function (col) {
-      col.classList.remove('over');
+    [c1,c2].forEach(function (container) {
+      container.classList.remove('over');
     });
+    
   }  
+  
+  var createChild = function(content){
+    var el = document.createElement("li");
+    el.id = "col-" + cols.length;
+    el.setAttribute("draggable", true);
+    el.innerHTML = content;
+    el.addEventListener('dragstart', handleDragStart, false);
+    el.addEventListener('dragend', handleDragEnd, false);    
+    
+    cols.push(el);
+    return el;
+  }
 
 
   var c1 = document.getElementById("container1");
   var c2 = document.getElementById("container2");  
-  var cols = document.querySelectorAll('#container1 li, #container2 li');
+  var cols = [].slice.call(document.querySelectorAll('#container1 li, #container2 li'));
   for(var i=0; i < cols.length; i++){
     var col = cols[i];
     var id = "col-" + i;
