1b8de86a2830f1bd34e4043e603b9d8a7a02fd65
Flurin Egger <flurin@digitpaint.nl>
2012-12-22 11:08:26 +0100

    Add the 22nd

--- html/22/22.js
+++ html/22/22.js
@@ -17,76 +17,86 @@ Copyright 2012 by Digitpaint. This code is licensed under the MIT License.
 document.addEventListener("DOMContentLoaded", function(){
   var lastEnter = null;
   
+  // =======================
+  // = Events on draggable =
+  // =======================
   
   var handleDragStart = function(e) {
-    this.style.opacity = '0.4';  // this / e.target is the source node.
+    this.style.opacity = '0.4';  
     
     // This is for internal use only
     e.dataTransfer.setData("domid", this.id);
     
-    // If you drag this out there we add the category too.
+    // If you drag this out of the browser we add the category too.
     var category = this.parentNode.previousElementSibling.innerText;
     e.dataTransfer.setData("text/plain", category + " : " + this.innerText);
     e.dataTransfer.setData("text/html", "<strong>" + category + "</strong> : " + this.innerHTML);    
 
   }
+  
+  var handleDragEnd = function(e) {
+    this.style.opacity = "1";
+
+    [c1,c2].forEach(function (container) {
+      container.classList.remove('over');
+    });
+    
+  }  
+
+  // =======================
+  // = Events on droppable =
+  // =======================
 
   var handleDragOver = function(e) {
-    e.preventDefault(); // Necessary. Allows us to drop.
+    // Drop doesn't work without preventing the default atoin.
+    e.preventDefault(); 
 
-    e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    // Set the effect (most of the times this means what kind of cursor to show)
+    if(e.dataTransfer.types.indexOf("domid") > -1){
+      // We got an existing node, so we'll move it
+      e.dataTransfer.dropEffect = 'move';
+    } else {
+      // This is a new node, so we'll copy it
+      e.dataTransfer.dropEffect = 'copy';
+    }
     
     // Hack to counter handleDragLeave being called when entering child nodes.
     this.classList.add('over');
     return false;
   }
 
-  var handleDragEnter = function(e) {
-    if (e.stopPropagation) {
-      e.stopPropagation(); // stops the browser from redirecting.
-    }    
-    // this / e.target is the current hover target.
+  var handleDragEnter = function(e) {   
+    e.stopPropagation(); // stops the browser from redirecting.
     this.classList.add('over');
   }
 
   var handleDragLeave = function(e) {
-    if (e.stopPropagation) {
-      e.stopPropagation(); 
-    }
-    
+    e.stopPropagation(); 
     this.classList.remove('over');
   }
   
   var handleDrop = function(e) {
-    // this / e.target is current target element.
-
-    if (e.stopPropagation) {
-      e.stopPropagation(); // stops the browser from redirecting.
-    }
+    e.stopPropagation(); // stops the browser from redirecting.
     e.preventDefault();
 
     // See the section on the DataTransfer object.
     var domId = e.dataTransfer.getData("domid");
     var el;
     
-    // if(e.dataTransfer.types && e.dataTransfer.types.length > 0){
-    //   for(var i=0; i < e.dataTransfer.types.length; i++){
-    //     console.log(e.dataTransfer.types[i], e.dataTransfer.getData(e.dataTransfer.types[i]));        
-    //   }
-    // }
-    
     if(domId){
+      // If we have our costum "domid" data in here
       el = document.getElementById(e.dataTransfer.getData("domid"));
       this.appendChild(el);
     } else {
-
       if(e.dataTransfer.files.length > 0){
+        // If files are being dropped
         for(var i=0; i < e.dataTransfer.files.length; i++){
           el = createChild(e.dataTransfer.files[i].name);
           this.appendChild(el);
         }
       } else {
-        el = createChild(e.dataTransfer.getData(e.dataTransfer.types && e.dataTransfer.types[0] || "Text"));
+        // All other stuff
+        el = createChild(e.dataTransfer.getData(e.dataTransfer.types && e.dataTransfer.types[0] || "text/plain"));
         this.appendChild(el);
       }
     }
@@ -96,22 +106,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
     return false;
   }
-
-  var handleDragEnd = function(e) {
-    // this/e.target is the source node.
-    this.style.opacity = "1";
-
-    [c1,c2].forEach(function (container) {
-      container.classList.remove('over');
-    });
-    
-  }  
   
+  // Create a new draggable element
   var createChild = function(content){
     var el = document.createElement("li");
     el.id = "col-" + cols.length;
     el.setAttribute("draggable", true);
     el.innerHTML = content;
+    
+    // Don't forget to attach the start/end handlers to the new element!
     el.addEventListener('dragstart', handleDragStart, false);
     el.addEventListener('dragend', handleDragEnd, false);    
     
@@ -119,6 +122,9 @@ document.addEventListener("DOMContentLoaded", function(){
     return el;
   }
 
+  // =================
+  // = Attach events =
+  // =================
 
   var c1 = document.getElementById("container1");
   var c2 = document.getElementById("container2");  
@@ -137,6 +143,26 @@ document.addEventListener("DOMContentLoaded", function(){
     container.addEventListener('drop', handleDrop, false);
     container.addEventListener('dragover', handleDragOver, false);
     
-  })
+  });
   
+  
+  // =====================
+  // = The drag-out demo =
+  // =====================
+  
+  [].forEach.call(document.querySelectorAll(".dragout"), function(file){
+    file.addEventListener("dragstart",function(e){
+      var fPath;
+      if(typeof file.dataset === "undefined") {
+        fPath = file.getAttribute("data-downloadpath");
+      } else {
+        fPath = file.dataset.downloadpath;
+      }
+      var fName = fPath.split("/");
+      fName = fName[fName.length - 1];
+      var fileDetails = "application/octet-stream:" + fName + ":" + window.location.protocol + "//" + window.location.host + fPath;
+      e.dataTransfer.setData("DownloadURL",fileDetails);
+      e.dataTransfer.setData("text/plain", this.innerHTML);
+    },false);
+  })
 });
